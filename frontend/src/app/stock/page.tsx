b55aed9f312f1f1b674a1c240d8c46cfe86f8267
HukukaichOu <kouki0802.ao@gmail.com>
2025-05-28 12:58:44 +0900

    frontent進めた

---
+++ frontend/src/app/stock/page.tsx
@@ -0,0 +1,11 @@
+import { DataTableDemo } from "@/components/data-table";
+
+const Stock = () => {
+  return (
+    <div className="border-black border-2 rounded-[5px] w-[600px] mx-auto mt-10 p-3">
+      <DataTableDemo />
+    </div>
+  );
+};
+
+export default Stock;
